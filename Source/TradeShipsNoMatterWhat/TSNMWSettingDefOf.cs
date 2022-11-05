a23f48aa675a13c3097b9faacd9318f733232382
Some Guy <sendm3@mst.edu>
2022-11-05 17:19:04 -0500

    compiled for 1.4 but there are still issues I need to work on

---
+++ Source/TradeShipsNoMatterWhat/TSNMWSettingDefOf.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using RimWorld;
+using Verse;
+namespace TradeShipsNoMatterWhat
+{
+    [DefOf]
+    public static class TSNMWSettingDefOf
+    {
+        public static TSNMWSettingDef defaultSettings;
+
+        static TSNMWSettingDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(TSNMWSettingDefOf));
+        }
+    }
+}
